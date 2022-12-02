fa6dc96afadb7f04028594c00a58fec6fd2487e7
Tom B. Parker <tombap8@hotmail.com>
2022-12-02 09:37:01 +0900

    ssss

--- 06.React학습/react-app/my-react/src/DC/Join.js
+++ 06.React학습/react-app/my-react/src/DC/Join.js
@@ -1,9 +1,4 @@
 import React, { useState } from 'react';
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
 import { Link } from 'react-router-dom'
 
 function Join() {
@@ -65,55 +60,50 @@ function Join() {
     const onSubmit = (e) => {
         if(validation()) return;
         
-        // API Call
-        
-
+        // 하단에 데이터 처리 페이지 호출
     }
 
     return (
         <div>
-            <Container className="panel">
-                <Form>
-                    <Form.Group as={Row} className="mb-3">
-                        <Col sm>
-                            <Form.Control maxLength={20} placeholder="아이디" value={userId} onChange={onChangeUserId} />
-                            {userIdError && <div className="invalid-input">사용자 ID는 5자 이상이어야 하며 문자 또는 숫자를 포함해야 합니다.</div>}
-                        </Col>
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3">
-                        <Col sm>
-                            <Form.Control maxLength={20} type="password" placeholder="비밀번호" value={password} onChange={onChangePassword} />
-                            {passwordError && <div className="invalid-input">비밀번호는 8자 이상이어야 하며 문자와 숫자를 각각 하나 이상 포함해야 합니다. </div>}
-                        </Col>
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3">
-                        <Col sm>
-                            <Form.Control maxLength={20} type="password" placeholder="비밀번호확인" value={confirmPassword} onChange={onChangeConfirmPassword} />
-                            {confirmPasswordError && <div className="invalid-input">비밀번호가 일치하지 않았습니다.</div>}
-                        </Col>
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3">
-                        <Col sm>
-                            <Form.Control maxLength={20} placeholder="이름" value={userName} onChange={onChangeUserName} />
-                            {userNameError && <div className="invalid-input">필수입력</div>}
-                        </Col>
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3">
-                        <Col sm>
-                            <Form.Control maxLength={50} type="input" placeholder="이메일주소" value={email} onChange={onChangeEmail} />
-                            {emailError && <div className="invalid-input">유효한 이메일 형식을 입력하십시오.</div>}
-                        </Col>
-                    </Form.Group>
-                    <br />
-                    <div className="d-grid gap-1">
-                        <Button variant="secondary" onClick={onSubmit}>
-                            회원가입
-                        </Button>
-                    </div>
-                </Form>
+            <form onSubmit={onSubmit}>
+                <div>
+                    <label> 아이디를 입력하세요
+                        <input type="text" maxLength="20" placeholder="아이디" value={userId} onChange={onChangeUserId} />
+                        {userIdError && <div className="invalid-input">사용자 ID는 5자 이상이어야 하며 문자 또는 숫자를 포함해야 합니다.</div>}
+                    </label>
+                </div>
+                <div>
+                    <label> 비밀번호를 입력하세요
+                        <input type="password" maxLength="20" placeholder="비밀번호" value={password} onChange={onChangePassword} />
+                        {passwordError && <div className="invalid-input">비밀번호는 8자 이상이어야 하며 문자와 숫자를 각각 하나 이상 포함해야 합니다. </div>}
+                    </label>
+                </div>
+                <div>
+                    <label> 비밀번호 확인을 입력하세요
+                        <input type="password" maxLength="20" placeholder="비밀번호확인" value={confirmPassword} onChange={onChangeConfirmPassword} />
+                        {confirmPasswordError && <div className="invalid-input">비밀번호가 일치하지 않았습니다.</div>}
+                    </label>
+                </div>
+                <div>
+                    <label> 이름을 입력하세요
+                        <input type="text" maxLength="20" placeholder="이름" value={userName} onChange={onChangeUserName} />
+                        {userNameError && <div className="invalid-input">필수입력</div>}
+                    </label>
+                </div>
+                <div>
+                    <label> 이메일을 입력하세요
+                        <input type="text" maxLength="50" placeholder="이메일주소" value={email} onChange={onChangeEmail} />
+                        {emailError && <div className="invalid-input">유효한 이메일 형식을 입력하십시오.</div>}
+                    </label>
+                </div>
                 <br />
-                <span className="text">이미 회원이신가요? <Link to="/login" className="link">로그인</Link></span>
-            </Container>
+                <div>
+                    <button onClick={onSubmit}>회원가입</button>
+                </div>
+            </form>
+            <br />
+            <span className="text">이미 회원이신가요? 
+            <Link to="/login" className="link">로그인</Link></span>
         </div>
     );
 }
